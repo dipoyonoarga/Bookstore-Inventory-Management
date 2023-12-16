82226c29fe90e8f19f1dfb299d1b4e4f24191071
Arga <dipoyonoarga@gmail.com>
2023-12-16 10:06:17 +0700

    add new feature at 10:06 16/12/23

--- bookstore_inventory_management.cpp
+++ bookstore_inventory_management.cpp
@@ -1,21 +1,83 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
+// Variabel untuk loop
+string loop;
+
+// Fungsi tampilkan semua buku
+void tampilkanDatabase(const vector<string> &databaseBuku)
+{
+    do
+    {
+        system("cls");
+        if (databaseBuku.empty())
+        {
+            cout << "Database buku kosong\n";
+        }
+        else
+        {
+            cout << "\nDatabase buku:\n"
+                 << endl;
+            for (const auto &buku : databaseBuku)
+            {
+                cout << buku << endl;
+            }
+        }
+        cout << "\nKetik t untuk kembali ke menu utama : ";
+        cin >> loop;
+    } while (loop == "y");
+}
+
+// Fungsi tambah buku
+void tambahBuku(vector<string> &databaseBuku)
+{
+    do
+    {
+        system("cls");
+        cout << "Tambah buku:"
+             << endl;
+        string bukuBaru;
+        string judul, penulis;
+
+        // Input judul buku
+        cout << "\nJudul buku: ";
+        cin.ignore();
+        getline(cin, judul);
+
+        // Input penulis buku
+        cout << "Penulis buku: ";
+        getline(cin, penulis);
+
+        // Menggabungkan judul dan penulis menjadi data buku baru
+        bukuBaru = judul + " - " + penulis;
+        databaseBuku.push_back(bukuBaru);
+
+        // Tambah buku lagi
+        cout << "\nBuku berhasil ditambahkan, tambah buku lagi? (y/t): ";
+        cin >> loop;
+    } while (loop == "y");
+}
 
 int main()
 {
+    // Simulasi database buku
+    vector<string> databaseBuku;
+    // Interface menu
     int menu;
     do
     {
         system("cls");
         cout << "\nProgram Manajemen Inventaris Toko Buku" << endl;
-        cout << "\n1. Tambah buku\n2. Cari buku\n3. Ubah data buku\n4. Tampilkan semua buku\n5. Keluar " << endl;
+        cout << "\n1. Tambah buku\n2. Cari buku\n3. Ubah data buku\n4. Tampilkan database\n5. Keluar " << endl;
         cout << "\nPilihan : ";
         cin >> menu;
 
         switch (menu)
         {
         case 1:
-            cout << "Tambah" << endl;
+            tambahBuku(databaseBuku);
             break;
         case 2:
             cout << "Cari buku" << endl;
@@ -24,15 +86,15 @@ int main()
             cout << "Edit" << endl;
             break;
         case 4:
-            cout << "Tampilkan" << endl;
+            tampilkanDatabase(databaseBuku);
             break;
         case 5:
-            cout << "" << endl;
+            cout << "\nProgram telah selesai" << endl;
             break;
         default:
             cout << "\nPilihan invalid";
             break;
         }
     } while (menu != 5);
-    cout << "Program telah selesai";
+    return 0;
 }
